d23946fc890a874b801b3ccfb5de7ae3a043f7cc
Hudson Hancock <hudsonhancock@gmail.com>
2021-07-18 21:21:27 -0400

    made files and wrote html and css for mobile

---
+++ Assets/app.js
@@ -0,0 +1,22 @@
+
+// function startTimer() {
+//     // Sets timer
+//     timer = setInterval(function() {
+//       timerCount--;
+//       timerElement.textContent = timerCount;
+//       if (timerCount >= 0) {
+//         // Tests if win condition is met
+//         if (isWin && timerCount > 0) {
+//           // Clears interval and stops timer
+//           clearInterval(timer);
+//           winGame();
+//         }
+//       }
+//       // Tests if time has run out
+//       if (timerCount === 0) {
+//         // Clears interval
+//         clearInterval(timer);
+//         loseGame();
+//       }
+//     }, 1000);
+//   }
