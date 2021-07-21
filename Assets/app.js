e9289196fe1778855550d983c36fdc0cd30efdff
Hudson Hancock <hudsonhancock@gmail.com>
2021-07-21 15:41:02 -0400

    redid data structure and changed styling

--- Assets/app.js
+++ Assets/app.js
@@ -1,22 +1,46 @@
+quizData = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        
+            a: '<scripting>',
+            b: '<js>',
+            c: '<script>',
+            d: 'javascript',
+        
+         correct: 'c',
 
-// function startTimer() {
-//     // Sets timer
-//     timer = setInterval(function() {
-//       timerCount--;
-//       timerElement.textContent = timerCount;
-//       if (timerCount >= 0) {
-//         // Tests if win condition is met
-//         if (isWin && timerCount > 0) {
-//           // Clears interval and stops timer
-//           clearInterval(timer);
-//           winGame();
-//         }
-//       }
-//       // Tests if time has run out
-//       if (timerCount === 0) {
-//         // Clears interval
-//         clearInterval(timer);
-//         loseGame();
-//       }
-//     }, 1000);
-//   }
+    }, {
+        question: 'The external JavaScript file must contain the <script> tag.',
+
+        a: 'False',
+        b: 'True',
+
+        correct: 'a'
+    }, {
+        question: 'How do you call a function named "myFunction"?',
+
+            a: 'myFunction()',
+            b: 'call my Function()',
+            c: 'call function myFunction()',
+
+        correct: 'a'
+    }, {
+        question: 'How do you call a function named "myFunction"?',
+
+        a: 'myFunction()',
+        b: 'call my Function()',
+        c: 'call function myFunction()',
+
+    correct: 'a'
+    }, {
+        question: 'How to write an IF statement in JavaScript?',
+
+        a: 'if i == 5 then',
+        b: 'if i = 5',
+        c: 'if i = 5 then',
+        d: 'if (i == 5)',
+
+    correct: 'd'
+    }
+]
+
